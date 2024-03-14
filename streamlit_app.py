94494c6a795cf938694b0181f8ec17f9888fd97d
LSchnatmann <163428545+LSchnatmann@users.noreply.github.com>
2024-03-14 15:51:26 +0100

    Update streamlit_app.py
    change fuityvice to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ st.write('The name on your Smoothie will be:', name_on_order)
 ingredients_list=st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 if ingredients_list:
